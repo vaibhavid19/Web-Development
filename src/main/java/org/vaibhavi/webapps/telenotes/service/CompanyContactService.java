1ff6534649b34694844e8ae80b0d9a1778e3aa4a
vaibhavid19 <vaibhavidoiphode@gmail.com>
2018-03-08 16:18:44 -0700

    Update CompanyContactService.java

--- src/main/java/org/vaibhavi/webapps/telenotes/service/CompanyContactService.java
+++ src/main/java/org/vaibhavi/webapps/telenotes/service/CompanyContactService.java
@@ -13,33 +13,14 @@ import org.vaibhavi.webapps.telenotes.model.User;
 
 
 public class CompanyContactService {
-	
-	//public static SessionFactory sessionFactory = new Configuration().configure(new File("D:/2018/workspace/telenotes/src/hibernate.cfg.xml")).buildSessionFactory();
-	//public static SessionFactory sessionFactory = new Configuration().configure(new File("D:/2018/workspace/telenotes/src/hibernate.cfg.xml")).buildSessionFactory();
-	
-		
+			
 	//This is just for initial testing, do not expose session to be static variable. 
 	//Close the connection and create different sessions as required
 	public static Session session = new Configuration().configure(new File("D:/2018/workspace/telenotes/src/hibernate.cfg.xml")).buildSessionFactory().openSession();
 	public CompanyContact companyRecord;
 	
 	public List<CompanyContact> getAllCompanyContacts() {
-		
-		//USE OF HQL TO FETCH ALL THE SQL QUERIES
-		/*List<CompanyContact> list = new ArrayList<CompanyContact>();
-		
-		CompanyContact c1 = new CompanyContact("netflix");
-		CompanyContact c2 = new CompanyContact("uber");
-		CompanyContact c3 = new CompanyContact("dropbox");
-		CompanyContact c4 = new CompanyContact("lyft");
-		CompanyContact c5 = new CompanyContact("pinterest");
-		list.add(c1);
-		list.add(c2);
-		list.add(c3);
-		list.add(c4);
-		list.add(c5);*/
-				
-		
+
 		session.beginTransaction();
 		Query query = session.createQuery("from CompanyContact");
 		List contacts = query.list();
@@ -69,20 +50,6 @@ public class CompanyContactService {
 		
 		return deleteCompanyContactFromDatabase(companyContact);
 	}
-	
-/*	This method will be obsolete as we only need list of all contacts for view
-	Unless changed to dto method
-	public CompanyContact getCompanyContactFromDatabase(CompanyContact companyContact) {
-		
-		try{		
-			companyRecord = (CompanyContact) session.get(CompanyContact.class, companyContact.getCompanyName());						
-			
-		} catch (Exception e) {
-	        System.out.println(e.getStackTrace());
-	        session.getTransaction().rollback();
-	    }
-		return companyRecord;
-	}*/
 		
 	public CompanyContact addCompanyContactToDatabase(CompanyContact companyContact) {
 
